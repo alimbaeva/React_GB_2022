ddaeb46dd85ea61d5da1a6fbda34c711f0201b5e
Asel <asel.alimbaeva.kk@gmail.com>
2022-03-02 15:32:30 +0300

    feat: add Massage component

--- gb_lesson-1/src/App.js
+++ gb_lesson-1/src/App.js
@@ -1,8 +1,10 @@
+import Message from "./component/Message";
 
 function App() {
+  let name = 'Asel';
   return (
     <div className="App">
-
+      <Message text={name} />
     </div>
   );
 }
---
+++ gb_lesson-1/src/component/Message.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Message({ text }) {
+    return (
+        <div>
+            <h1>Привет, {text}</h1>
+        </div>
+    )
+}
+
+export default Message
