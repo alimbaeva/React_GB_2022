e98342ff1c9345af553ac1bd53e032bb916824d0
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-02 11:11:44 +0300

    feat: count

--- redux-count-l6/src/App.js
+++ redux-count-l6/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const count = useSelector(state => {
+    return state
+  });
+
+  const dispatch = useDispatch();
+
+  const increate = () => {
+    dispatch({ type: 'plus' })
+  }
+  const descreate = () => {
+    dispatch({ type: 'minus' })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="count">
+      <div className="num">
+        {count}
+      </div>
+      <button onClick={() => increate()}>Увеличить счетчик</button>
+      <button onClick={() => descreate()}>Уменьшить счетчик</button>
+      {/* <button onClick={() => dispatch({ type: 'plus' })}>Увеличить счетчик</button>
+      <button onClick={() => dispatch({ type: 'minus' })}>Уменьшить счетчик</button> */}
     </div>
   );
 }
--- redux-count-l6/src/index.js
+++ redux-count-l6/src/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialState = 0;
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    case 'plus':
+      return state + 1;
+
+    case "minus":
+      return state - 1;
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
