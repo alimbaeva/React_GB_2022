4f6c1680fe40121d2e0f965baa7a3bd0523313d2
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-08 16:35:49 +0300

    feat: middleware

---
+++ lesson-7-thanck-2/src/redux/store/configureStore.jsx
@@ -0,0 +1,13 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { CountReduser } from './reducer/CountReduser';
+import { loggerMiddleware } from './reducer/CountReduser';
+import { countMiddleware } from './reducer/CountReduser';
+
+export const rootReducer = combineReducers({
+    count: CountReduser
+});
+
+const thunkMiddleware = store => next => action => {
+    typeof action === 'function' ? action(store.dispatch, store.getState) : next(action)
+}
+export const store = createStore(rootReducer, applyMiddleware(countMiddleware))
