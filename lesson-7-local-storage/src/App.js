448c7da6f44750f317d3417d528bc50b5f086afe
Asel <asel.alimbaeva.kk@gmail.com>
2022-04-08 17:55:18 +0300

    feat: local stor redux

---
+++ lesson-7-local-storage/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+// import { getDate } from "./redux/store/reducer/action";
+import { CountSelector } from './redux/store/reducer/count/CountSelector'
+
+function App() {
+  const counter = useSelector(CountSelector);
+  const dispatch = useDispatch();
+
+
+  return (
+    <div className="App">
+      {
+        <div>
+          {counter}
+        </div>
+      }
+      <button onClick={() => dispatch({ type: 'plus' })}> Увеличить</button>
+      <button onClick={() => dispatch({ type: 'minus' })}> Уменьшить</button >
+    </div >
+  );
+}
+
+export default App;
---
+++ lesson-7-local-storage/src/redux/store/reducer/CountReduser.jsx
@@ -0,0 +1,43 @@
+import { store } from "../configureStore";
+
+const initialState = {
+    counter: 0
+};
+
+export const CountReduser = (state = initialState, action) => {
+    switch (action.type) {
+        case 'plus':
+            return {
+                ...state,
+                counter: state.counter + 1
+            };
+        case 'minus':
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+        default:
+            return state
+    }
+}
+
+export const loggerMiddleware = store => next => action => {
+    let result;
+    console.groupCollapsed('Dispatching', action.type);
+    console.log('Prev state', store.getState());
+    console.log('Action', action)
+    result = next(action);
+    console.log('next state', store.getState());
+    console.groupEnd();
+    return result;
+}
+
+
+export const countMiddleware = store => next => action => {
+    if (action.type === 'minus') {
+        setTimeout(() => {
+            console.log('Уменьшено')
+        }, 4000)
+    }
+    return next(action)
+}
